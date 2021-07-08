9fb404ce95899d6b74c39ebe518062c7469744ea
Evgeniy Khristenko <e.khristenko@ostrovok.ru>
2021-07-08 23:37:27 +0300

    add more comments

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 const titlesBlock = document.querySelector('#titles');
 const selectElement = document.querySelector('#language');
 
-const DEFAULT_LANGUAGE = 'ru';
+const DEFAULT_LANGUAGE = 'ru'; // всегда нужен какой-то дефолт, на случай если основные данные не придут или будут неверны
 const titles = Object.keys(data); // возвращает все ключи обьекта в виде массива http://old.code.mu/javascript/object/Object.keys.html
 const supportedLanguages = ['en', 'ru', 'ua'];
 
 function getLanguage () {
-  const formattedLanguageFromHash = window.location.hash.slice(1); // .slice(1) убирает первый символ из строки
+  /* .slice(1) убирает первый символ из строки, нам это нужно, потому что {window.location.hash} возвращает хэш в формате: #{наш хэш}, а нам # не нужен */
+  const formattedLanguageFromHash = window.location.hash.slice(1);
   return supportedLanguages.includes(formattedLanguageFromHash) ? formattedLanguageFromHash : DEFAULT_LANGUAGE;
 };
 
-function renderOptions (selectedLanguage) { // проходимся по массиву {supportedLanguages} и добавляем каждый язык в {selectElement}
+// проходимся по массиву {supportedLanguages} и добавляем каждый язык в {selectElement}
+function renderOptions (selectedLanguage) {
   selectElement.textContent = '';
 
-  supportedLanguages.forEach(function (language) { // forEach проходится по каждому елементу массива и кладет его в переданную функцию 
+  supportedLanguages.forEach(function (language) { // forEach проходится по каждому елементу массива и кладет его в переданную функцию
+
+    /*
+      Для создания и запихивания html-элементов можно также юзать такой способ: selectElement.innerHTML += '<option>бла бла</option>'
+      Но .innerHTML более медленный, т.к полность перезаписывает контент блока к которому применяется, когда .appendChild лишь добавляет элемент в конец.
+    */
     const optionElement = document.createElement('option');
     const isSelected = language === selectedLanguage;
   
@@ -38,13 +45,13 @@ function renderTitles (data, language) { // проходимся по обьек
   });
 };
 
-selectElement.addEventListener('change', function () {  // когда меняем в селекте язык - меняем хэш в url
+selectElement.addEventListener('change', function () {  // когда меняем в селекте язык - меняем хэш в url, чтобы была синхронизация данных в приложении
   window.location.hash = selectElement.value;
 
   renderOptions(selectElement.value);
 });
 
-window.addEventListener('hashchange', function () { // когда хэш url меняется перерерисовываем загаловки в правильном языке
+window.addEventListener('hashchange', function () { // когда хэш url меняется перерерисовываем загаловки в правильном языке и элементы селекта
   const language = getLanguage();
 
   renderTitles(data, language);
